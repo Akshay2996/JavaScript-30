f94bbb91489ed8465b458841e617ccbc83b691b1
Akshay Sharma <37118877+Akshay2996@users.noreply.github.com>
2020-01-24 09:04:03 +0530

    :alembic: Shift & See

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* Get our Elements*/
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+/* Build out functions*/
+
+/* Hook up the Event Listeners*/
