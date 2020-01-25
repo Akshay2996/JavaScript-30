ada497b471caac88050280d6ee1aaf682424e53e
Akshay Sharma <37118877+Akshay2996@users.noreply.github.com>
2020-01-25 13:06:00 +0530

    :construction: Work in Progress

--- scripts.js
+++ scripts.js
@@ -8,5 +8,24 @@ const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
 /* Build out functions*/
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  // Alternate way is Ternary operator
+  // const method = video.paused ? 'play' : 'pause';
+  // video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚❚";
+  toggle.textContent = icon;
+}
 
 /* Hook up the Event Listeners*/
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+toggle.addEventListener("click", togglePlay);
