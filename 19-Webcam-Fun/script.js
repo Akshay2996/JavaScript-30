c920a4770630960ce30ea0c29e5251542e019dd7
Akshay Sharma <akshaysharmaay@gmail.com>
2020-08-20 12:31:23 +0530

    Fixing the issue

--- 19-Webcam-Fun/script.js
+++ 19-Webcam-Fun/script.js
@@ -4,7 +4,6 @@ const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
 
-// Getting video from webcam
 function getVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
@@ -14,11 +13,10 @@ function getVideo() {
       video.play();
     })
     .catch(err => {
-      console.error(`OH NO!!!`, err);
+      console.error("Oh, NOOO!!!", err);
     });
 }
 
-// Taking video from webcam to an empty canvas
 function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -27,7 +25,7 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
+    //   take pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
     // mess with them
     // pixels = redEffect(pixels);
@@ -35,48 +33,12 @@ function paintToCanvas() {
     pixels = rgbSplit(pixels);
     ctx.globalAlpha = 0.8;
 
-    // pixels = greenScreen(pixels);
     // put them back
     ctx.putImageData(pixels, 0, 0);
+    // debugger;
   }, 16);
 }
 
-// Taking photo by clicking the button
-function takePhoto() {
-  // played the sound
-  snap.currentTime = 0;
-  snap.play();
-
-  // take the data out of the canvas
-  const data = canvas.toDataURL("image/jpeg");
-  const link = document.createElement("a");
-  link.href = data;
-  link.setAttribute("download", "handsome");
-  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
-  strip.insertBefore(link, strip.firstChild);
-}
-
-// Red effect given
-function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
-  }
-  return pixels;
-}
-
-// RGBSplit effect
-function rgbSplit(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
-  }
-  return pixels;
-}
-
-// Greenscreen effect
 function greenScreen(pixels) {
   const levels = {};
 
@@ -84,10 +46,11 @@ function greenScreen(pixels) {
     levels[input.name] = input.value;
   });
 
-  for (let i = 0; i < pixels.data.length; i = i + 4) {
-    let red = pixels.data[i + 0];
-    let green = pixels.data[i + 1];
-    let blue = pixels.data[i + 2];
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
 
     if (
       red >= levels.rmin &&
@@ -102,6 +65,37 @@ function greenScreen(pixels) {
     }
   }
 
+  return pixels;
+}
+
+function takePhoto() {
+  // play sound
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt='Smart Man' />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] += 100; //r
+    pixels.data[i + 1] -= 50; //g
+    pixels.data[i + 2] *= 0.5; //b
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
   return pixels;
 }
 
